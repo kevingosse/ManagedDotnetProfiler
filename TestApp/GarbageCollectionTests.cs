0ba858caa6418ff34d1d19976a209d21948b3105
Kevin Gosse <krix33@gmail.com>
2024-12-01 20:54:26 +0100

    Upgrade to .NET 9

--- TestApp/GarbageCollectionTests.cs
+++ TestApp/GarbageCollectionTests.cs
@@ -8,14 +8,9 @@ internal class GarbageCollectionTests
 
         GC.Collect(2, GCCollectionMode.Default, blocking: true); // reason == COR_PRF_GC_INDUCED
 
-        // For some weird reason, the GC is not reported as induced if blocking is false
-        // https://github.com/dotnet/runtime/issues/86541
-        GC.Collect(2, GCCollectionMode.Default, blocking: false); // reason == COR_PRF_GC_OTHER
-
         var logs = Logs.Fetch().ToList();
 
         Logs.AssertContains(logs, "GarbageCollectionStarted - 0, 1, 2, 3, 4 - COR_PRF_GC_INDUCED - 1");
-        Logs.AssertContains(logs, "GarbageCollectionStarted - 0, 1, 2, 3, 4 - COR_PRF_GC_OTHER - 1");
         Logs.AssertContains(logs, "GarbageCollectionFinished - 0");
 
         Logs.AssertContains(logs, "RuntimeSuspendStarted - COR_PRF_SUSPEND_FOR_GC");
--- TestApp/launch.cmd
+++ TestApp/launch.cmd
@@ -1,4 +1,4 @@
 @set CORECLR_ENABLE_PROFILING=1
 @set CORECLR_PROFILER={846F5F1C-F9AE-4B07-969E-05C26BC060D8}
-@set CORECLR_PROFILER_PATH=E:\git\ManagedDotnetProfiler\ManagedDotnetProfiler\bin\Release\net8.0\win-x64\publish\ManagedDotnetProfiler.dll
+@set CORECLR_PROFILER_PATH=E:\git\ManagedDotnetProfiler\ManagedDotnetProfiler\bin\Release\net9.0\win-x64\publish\ManagedDotnetProfiler.dll
 @TestApp.exe
